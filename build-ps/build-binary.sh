dd6fa8573a68651b92524262969941a5d3fb12fa
Vadim Yalovets <vadim.yalovets@percona.com>
2024-11-21 13:46:12 +0200

    PKG-299 Release tasks for PS 8.4.2 pro

--- build-ps/build-binary.sh
+++ build-ps/build-binary.sh
@@ -230,8 +230,8 @@ if [ -n "$(command -v rpm)" ]; then
   if test "x$CMAKE_BUILD_TYPE" = "xDebug"
   then
     COMMON_FLAGS=`echo " ${COMMON_FLAGS} " | \
-              sed -e 's/ -O[0-9]* / /' \
-                  -e 's/-Wp,-D_FORTIFY_SOURCE=2/ /' \
+              sed -e 's/-Wall/-Wall -Wno-error=stringop-overflow -Wno-error=restrict -Wno-error=maybe-uninitialized -Wno-error=array-bounds -Wno-error=alloc-size-larger-than= -Wno-error=stringop-truncation/' \
+                  -e 's/-Wp,-D_FORTIFY_SOURCE=2//' \
                   -e 's/ -unroll2 / /' \
                   -e 's/ -ip / /' \
                   -e 's/^ //' \
